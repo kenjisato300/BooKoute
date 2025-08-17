5d5df3efa276a53ee4a0757371f958e5a2f6c5a9
kenji <sa10.sa10.ken@gmail.com>
2025-08-17 17:58:32 +0900

    Initial Commit

---
+++ BooKoute/BooKouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooKouteApp.swift
+//  BooKoute
+//
+//  Created by sato kenji on 2025/08/17.
+//
+
+import SwiftUI
+
+@main
+struct BooKouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BooKouteTests/BooKouteTests.swift
@@ -0,0 +1,17 @@
+//
+//  BooKouteTests.swift
+//  BooKouteTests
+//
+//  Created by sato kenji on 2025/08/17.
+//
+
+import Testing
+@testable import BooKoute
+
+struct BooKouteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

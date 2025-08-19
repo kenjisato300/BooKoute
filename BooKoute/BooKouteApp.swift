fdfcc9c67197a6ac74a495ade84ab1a34d1bf245
kenji <sa10.sa10.ken@gmail.com>
2025-08-19 10:28:16 +0900

    初回コミット: BooKoute プロジェクト作成（HomeScene 起動）

--- BooKoute/BooKouteApp.swift
+++ BooKoute/BooKouteApp.swift
@@ -1,17 +1,10 @@
-//
-//  BooKouteApp.swift
-//  BooKoute
-//
-//  Created by sato kenji on 2025/08/17.
-//
-
 import SwiftUI
 
 @main
 struct BooKouteApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeScene()
         }
     }
 }
---
+++ BooKoute/Scenes /HomeScene.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct HomeScene: View {
+    var body: some View {
+        NavigationView {
+            Text("ホーム画面")
+                .navigationTitle("BooKoute")
+        }
+    }
+}
+
+#Preview {
+    HomeScene()
+}
